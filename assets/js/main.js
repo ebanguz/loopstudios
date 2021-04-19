0d0f1ca15214189aceb5aa1353f5cf1688034154
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2021-04-19 18:01:09 -0400

    style: header completed and menu toggler

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", e => {
+    const $nav = document.querySelector(".nav");
+    const $hamburger_button = document.querySelector(".nav__hamburguer");
+
+    $hamburger_button.addEventListener("click", e => {
+        $nav.classList.toggle("open");
+        document.body.classList.toggle("no-scroll");
+        $hamburger_button.classList.toggle("opened");
+        $hamburger_button.setAttribute(
+            "aria-expanded",
+            this.classList.contains("opened")
+        );
+    });
+});
+
+/*
+
+
+
+*/
